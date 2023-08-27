febb2410dce594c775ef929bb2d21acf83d40218
Albin Robert <albinrobert61@gmail.com>
2023-08-27 00:08:02 -0400

    cloud config done

--- server.js
+++ server.js
@@ -67,15 +67,19 @@ app.get('/authUser', (req, res) => {
 })
 
 
-//if in production, it executes
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'));
+app.get("/", (req, res) =>{
+    res.json({message:"docker is tough"})
+})
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+// //if in production, it executes
+// if (process.env.NODE_ENV === 'production') {
+//     //set static folder
+//     app.use(express.static('client/build'));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
 
 const PORT = process.env.PORT || 5000;
 
