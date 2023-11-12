b2a725914cdbaaa0068ce4ecbbfe870e5897af1c
Albin Robert <albinrobert61@gmail.com>
2023-11-12 14:43:09 -0500

    fixed shellscript syntax error

--- nginx/certcheck.sh
+++ nginx/certcheck.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -e "/etc/letsencrypt/live/samplesapp.net/privkey.pem"] && [-e "/etc/letsencrypt/live/samplesapp.net/fullchain.pem"]; then 
+if [ -e "/etc/letsencrypt/live/samplesapp.net/privkey.pem" ] && [ -e "/etc/letsencrypt/live/samplesapp.net/fullchain.pem" ]; then
     echo "Files exist"
     rm "/etc/nginx/conf.d/temp.conf"
 else
     echo "Files don't exist"
     rm "/etc/nginx/conf.d/app.conf"
-fi 
+fi
 
 rm "/etc/nginx/conf.d/default.conf"
